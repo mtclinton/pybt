568aa278f126b234f627be2ddab3d57adfd98476
Max Clinton <maxtclinton@gmail.com>
2023-03-08 23:47:16 -0500

    Adding torrent data structures

--- pybt/torrentfile/torrentfile.py
+++ pybt/torrentfile/torrentfile.py
@@ -2,6 +2,33 @@
 from pbencode import decode as bencode_decode
 
 
+class TorrentFile:
+    """Representation of a torrent file"""
+
+    announce: str
+    infohash: bytes
+    piecehashes: list
+    piecelength: int
+    length: int
+    name: str
+
+
+class BencodeInfo:
+    """Tracking the download of a torrent"""
+
+    pieces: bytes
+    piecelength: int
+    length: int
+    name: str
+
+
+class BencodeTorrent:
+    """Tracking the download of a torrent and how to tell peers"""
+
+    announce: str
+    info: BencodeInfo
+
+
 def open_file(path: str):
     """Attempt to open and read torrent file"""
     with open(path, "rb") as torrent_file:
